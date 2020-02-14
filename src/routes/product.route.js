f02f5b43fe6a4992d65705cc036b408db9a12aa2
unknown <fegoworks@hotmail.com>
2020-02-14 10:20:21 +0100

    feat(validation-middleware): add middleware to validate request bodies

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,21 +3,27 @@ import productController from '../controllers/product.controller';
 import auth from '../middlewares/auth/auth.middleware';
 import permissions from '../middlewares/auth/role.middleware';
 import upload from '../middlewares/multer';
+import validate from '../helpers/validator';
 
 const router = express.Router();
 
 router.post('/products/',
   auth.verify,
   permissions.adminOnly,
-  upload.single('image'),
+  validate.validateBody(validate.schemas.productSchema),
   productController.addProduct);
 
 router.patch('/products/:productId',
   auth.verify,
   permissions.adminOnly,
-  upload.single('image'),
+  validate.validateBody(validate.schemas.productSchema),
   productController.editProduct);
 
+router.patch('/products/:productId/upload',
+  auth.verify,
+  permissions.adminOnly,
+  upload.single('image'),
+  productController.uploadProductImage);
 
 router.get('/products',
   auth.verify,
